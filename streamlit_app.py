f3fbbf1a1a6faa5bd800c51ea0863cecca4598ff
cthsnowflake <105894348+cthsnowflake@users.noreply.github.com>
2022-05-24 21:51:58 -0600

    Update streamlit_app.py
    Remove raw json and prepare for hardcoded fruit to be a variable instead.

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,13 +20,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
-
 # New Section to display fruityvice api response
 streamlit.header('Fruitvice Fruit Advice!')
 
 import requests
-fruityvice_response = requests.get("https://www.fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) # just writes the data to the screen
+fruityvice_response = requests.get("https://www.fruityvice.com/api/fruit/" + "kiwi")
+# streamlit.text(fruityvice_response.json()) # just writes the data to the screen
 
 # take the json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
