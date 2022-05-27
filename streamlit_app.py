c01551105f4698e2e4247972009f98a39182a1f1
cthsnowflake <105894348+cthsnowflake@users.noreply.github.com>
2022-05-26 23:20:18 -0600

    Update streamlit_app.py
    Setting up insert into Snowflake in the streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,3 +48,6 @@ streamlit.dataframe(my_data_rows)
 streamlit.header('Fruitvice Fruit Advice!')
 add_my_fruit = streamlit.text_input("What fruit would you like to add?",'Jackfruit')
 streamlit.write('Thanks for adding', add_my_fruit)
+
+# This won't work but whatevs
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
